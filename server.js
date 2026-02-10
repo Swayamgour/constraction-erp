edf61dce19b4fc4d6666e3e7c4ba979d210b77d4
Swayam Gaur <goura0775@gmail.com>
2026-02-10 12:10:16 +0530

    add

--- server.js
+++ server.js
@@ -85,6 +85,12 @@ app.get("/api", (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
